e0d21c4457139cac03e404261efebd5de7ecdcf4
abdallah-alaff96 <abdallah96_alaff@hotmail.com>
2022-02-10 13:51:32 +0200

    Add Row.css (initial styles) and Modifiying Row.js

--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "./axios";
+import "./Row.css";
+
+// From API Docs
+const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
@@ -7,6 +11,7 @@ function Row({ title, fetchUrl }) {
   useEffect(() => {
     async function fetchData() {
       const response = await axios.get(fetchUrl);
+
       setMovies(response.data.results);
       return response;
     }
@@ -16,8 +21,21 @@ function Row({ title, fetchUrl }) {
   console.log(movies);
 
   return (
-    <div>
-      <h1>{title}</h1>
+    <div className="row">
+      <h1 className="row__title">{title}</h1>
+
+      <div className="row__posters">
+        {/* several row posters */}
+
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className="row__poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
     </div>
   );
 }
