22de71abacab7840b8f251c9accd70b9001f999c
abdallah-alaff96 <abdallah96_alaff@hotmail.com>
2022-02-10 22:45:20 +0200

    Editting Banner component-edit#2 (add css styles & add logic and shap on the component

--- src/Banner.js
+++ src/Banner.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Banner.css";
 
+const base_url = "https://image.tmdb.org/t/p/original/";
+
 function Banner({ fetchUrl }) {
   const [movie, setMovie] = useState([]);
 
@@ -20,9 +22,36 @@ function Banner({ fetchUrl }) {
     fetchData();
   }, []);
 
-  console.log(movie);
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  };
 
-  return <header>hi</header>;
+  return (
+    <header
+      className="banner"
+      style={{
+        // backgroundSize: "cover",
+        backgroundImage: `url(${base_url}${movie?.backdrop_path})`,
+        // backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner__contents">
+        {/* title */}
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+
+        {/* div 2 buttons */}
+        <div>
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+
+        {/* description */}
+        <p className="banner__description">{truncate(movie?.overview, 150)}</p>
+      </div>
+    </header>
+  );
 }
 
 export default Banner;
